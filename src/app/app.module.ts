098251b3d5e88bbad5132621987849181e8c0ebb
Rohit Baskar <Rohit.Baskar@ey.com>
2020-02-17 14:00:14 -0500

    added components for home screen, score and standings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { StandingsComponent } from './standings/standings.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    StandingsComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
