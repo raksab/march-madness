8d980b5e71c6429bfbc3aea4737ba3f6e4eccf32
Rohit Baskar <Rohit.Baskar@ey.com>
2020-02-19 15:34:39 -0500

    continued work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StandingsComponent } from './standings/standings.component';
+import { ScoreComponent } from './score/score.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+},
+{
+    path: 'home',
+    component: HomeComponent,
+},
+{
+    path: 'standings',
+    component: StandingsComponent,
+},
+{
+    path: 'score', 
+    component: ScoreComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,8 @@ import { ScoreComponent } from './score/score.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
